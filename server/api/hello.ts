f8fd64d4f54292d2cd6694b1052a094dd7c3d6e4
Ivan Mokhonko <ivan.mokhonko@onereach.com>
2024-11-06 23:13:06 +0200

    1

--- server/api/hello.ts
+++ server/api/hello.ts
@@ -1,17 +1,16 @@
-import { DynamoDBClient, ScanCommand } from "@aws-sdk/client-dynamodb";
+// import { DynamoDBClient, ScanCommand } from "@aws-sdk/client-dynamodb";
 
-const client = new DynamoDBClient({ region: 'eu-central-1' });
+// const client = new DynamoDBClient({ region: 'eu-central-1' });
+
+import { secret } from '@aws-amplify/backend';
 
 export default defineEventHandler(async (event) => {
-  const input = {
-    TableName: "test-table"
-  };
-
-  const command = new ScanCommand({ TableName: "test-table"});
-  const { Items = [] } = await client.send(command);
+  // const command = new ScanCommand({ TableName: "test-table"});
+  // const { Items = [] } = await client.send(command);
 
   return {
-    items: Items?.[0],
+    secret: secret('secret'),
+    // items: Items?.[0],
     message: 'dev hello world'
   }
 })
