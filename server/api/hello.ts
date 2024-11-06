b4f43f6a7c3f7db7441af05aedb0dade4a50c250
Ivan Mokhonko <ivan.mokhonko@onereach.com>
2024-11-06 22:45:24 +0200

    feat: add dynamodb request

--- server/api/hello.ts
+++ server/api/hello.ts
@@ -1,5 +1,17 @@
-export default defineEventHandler((event) => {
+import { DynamoDBClient, ScanCommand } from "@aws-sdk/client-dynamodb";
+
+const client = new DynamoDBClient({ region: 'eu-central-1' });
+
+export default defineEventHandler(async (event) => {
+  const input = {
+    TableName: "test-table"
+  };
+
+  const command = new ScanCommand({ TableName: "test-table"});
+  const { Items = [] } = await client.send(command);
+
   return {
+    items: Items?.[0],
     message: 'dev hello world'
   }
 })
